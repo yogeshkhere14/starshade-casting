7bd148fb888db0964ed46223b6721d070b408dda
Yogesh <ysingh1217@gmail.com>
2025-07-07 01:34:05 +0530

    minor bug fixes

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  images: {
+    unoptimized: true
+  },
   output: 'export',
   experimental: {
     outputFileTracingIncludes: {
